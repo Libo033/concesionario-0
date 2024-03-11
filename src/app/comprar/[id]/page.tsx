2b2fdc376f834d1bcacdbb97584a4edee1106c35
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-11 15:18:17 -0300

    Creado pagina de comprar

---
+++ src/app/comprar/[id]/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "../page.module.css";
+
+const car = {
+  _id: "4",
+  main: "Honda S2000 2007",
+  second: "Perfecto estado. Detalles minimos. Transimision manual.",
+  image: [
+    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1709924045/06-concesionario/odzpz9qecs0ui6xxe83f.jpg",
+    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1710180918/06-concesionario/ijyreqzvnkt7380urshi.jpg",
+    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1710180922/06-concesionario/xm6fc926dvmfm0is83cq.jpg",
+    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1710180925/06-concesionario/xruzscfhhi3otpv6h7n1.jpg",
+  ],
+  km: 24000,
+  ubi: "Nuñes, Buenos Aires",
+};
+
+const ComprarID = ({ params }: { params: { id: string } }) => {
+  return (
+    <div className={styles.ComprarID}>
+      <p>{params.id}</p>
+    </div>
+  );
+};
+
+export default ComprarID;
