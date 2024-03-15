93fcb0e264984e92c70003d31b104f71db97dedb
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-14 21:11:35 -0300

    Implementado CarContext en page id

--- src/app/comprar/[id]/page.tsx
+++ src/app/comprar/[id]/page.tsx
@@ -1,64 +1,54 @@
 "use client";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "../page.module.css";
 import SwiperComprar from "@/componentes/others/SwiperComprar";
 import SideDataCard from "@/componentes/cards/SideDataCard";
 import BottomDataCard from "@/componentes/cards/BottomDataCard";
 import BasicInfo from "@/componentes/others/BasicInfo";
-
-// https://carfromjapan.com/cheap-used-honda-s2000-2007-for-sale-64e514651e29ffc2795fafac
-
-const car = {
-  _id: "4",
-  brand: "Honda",
-  model: "S2000",
-  year: 2007,
-  second: "Perfecto estado. Detalles minimos. Transimision manual.",
-  image: [
-    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1709924045/06-concesionario/odzpz9qecs0ui6xxe83f.jpg",
-    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1710180918/06-concesionario/ijyreqzvnkt7380urshi.jpg",
-    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1710180922/06-concesionario/xm6fc926dvmfm0is83cq.jpg",
-    "https://res.cloudinary.com/dsuydyqgz/image/upload/v1710180925/06-concesionario/xruzscfhhi3otpv6h7n1.jpg",
-  ],
-  km: 24000,
-  province: "Buenos Aires",
-  localidad: "Nuñes",
-  price: 25000,
-  exchange: "U$D",
-  motor: "2.2L",
-  transmision: "Manual",
-};
+import { CarContext } from "@/context/CarContext";
+import { ICar } from "@/libs/interfaces";
 
 const ComprarID = ({ params }: { params: { id: string } }) => {
+  const { cars, loadCars } = useContext(CarContext);
+  const [car, setCar] = useState<ICar>();
+
+  useEffect(() => {
+    if (loadCars) {
+      setCar(cars.find((c) => c._id === params.id));
+    }
+  }, [loadCars]);
+
   return (
     <div className={styles.ComprarID}>
-      <div className={styles.ComprarID_Top}>
-        <SwiperComprar images={car.image} alt={car.brand} />
-        <SideDataCard
-          km={car.km}
-          province={car.province}
-          year={car.year}
-          brand={car.brand}
-          model={car.model}
-          price={car.price}
-          exchange={car.exchange}
-          motor={car.motor}
-          transmision={car.transmision}
-        />
-        <BottomDataCard
-          brand={car.brand}
-          model={car.model}
-          exchange={car.exchange}
-          price={car.price}
-        />
-      </div>
-      <div className={styles.ComprarID_Bottom}>
-        <BasicInfo />
-      </div>
+      {car && (
+        <>
+          <div className={styles.ComprarID_Top}>
+            <SwiperComprar images={car.imagenes} alt={car.marca} />
+            <SideDataCard
+              km={car.km}
+              province={car.provincia}
+              year={car.ano}
+              brand={car.marca}
+              model={car.modelo}
+              price={car.precio}
+              exchange={car.moneda}
+              motor={car.informacion.motor}
+              transmision={car.informacion.transmision}
+            />
+            <BottomDataCard
+              brand={car.marca}
+              model={car.modelo}
+              exchange={car.moneda}
+              price={car.precio}
+            />
+          </div>
+          <div className={styles.ComprarID_Bottom}>
+            <BasicInfo />
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
 export default ComprarID;
-
-// <Image src={car.image[0]} alt={car.main} width={800} height={800} />
