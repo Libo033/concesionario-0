f137d7ba0a113c224afd02e7a5a2cbe1ba2536e7
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-16 10:10:57 -0300

    Creado: support

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./page.module.css";
+import { Button, TextField } from "@mui/material";
 
 const Support = () => {
-  return <div>Support</div>;
+  return (
+    <div className={styles.Support}>
+      <p className={styles.Support_Title}>Soporte</p>
+      <div className={styles.Support_Container}>
+        <form className={styles.Support_Form}>
+          <TextField fullWidth label="Razon" variant="outlined" />
+          <TextField
+            fullWidth
+            label="Mensaje"
+            multiline
+            rows={6}
+            variant="outlined"
+          />
+          <Button fullWidth variant="contained">
+            enviar
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Support;
