7a437642bb4ae09a4de1320d59f80b791d8ef867
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-18 15:32:04 -0300

    Fixeado errores en NavigationDrawer

--- src/componentes/navigation/NavigationDrawer.tsx
+++ src/componentes/navigation/NavigationDrawer.tsx
@@ -1,14 +1,22 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
 import { Button } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const NavigationDrawer = () => {
+  const r = useRouter();
+
   return (
     <div className={styles.NavigationDrawer}>
       <div className={styles.NavigationDrawer_Button}>
-        <Button fullWidth variant="contained">
+        <Button
+          onClick={() => r.push("/account/login")}
+          fullWidth
+          variant="contained"
+        >
           ingresar
         </Button>
       </div>
